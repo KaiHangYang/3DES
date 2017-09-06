ead0ad3c13cc6a75b68f0becea3de2c24a2d1a33
kaihang <y907987244@gmail.com>
2017-09-06 17:00:20 +0800

    Find the problem of the energys. You can see the commit to get what I have changed.

--- src/vnect/mFittingUtils.cpp
+++ src/vnect/mFittingUtils.cpp
@@ -11,9 +11,9 @@
 namespace mFitting {
     // This may be implemented by lossfunction
     const double m_fitting_w1 = 1;
-    const double m_fitting_w2 = 44;
-    const double m_fitting_w3 = 0.07;
-    const double m_fitting_w4 = 0.11;
+    const double m_fitting_w2 = 6.633249581; // sqrt(44)
+    const double m_fitting_w3 = 0.264575131; // sqrt(0.07)
+    const double m_fitting_w4 = 0.331662479; // sqrt(0.11)
     
     double base_plane_height =  5.0 * glm::tan(glm::radians(base_vof/2));
     double base_plane_width = ratio_w * base_plane_height / ratio_h;
@@ -53,57 +53,89 @@ namespace mFitting {
         int num;// the number of the point 
     };
     struct EPROJError {
-        EPROJError(double * joints_2d, glm::mat4 mvp): joints_2d(joints_2d), MVP(mvp){};
+        EPROJError(double kl_x, double kl_y, int num, glm::mat4 mvp): kl_x(kl_x), kl_y(kl_y), num(num), MVP(mvp){};
         template<typename T> bool operator() (const T * const theta, const T * const d, T * residuals) const {
             T global_3d[joint_num*3];
-            cal_3djoints(theta, d, global_3d, -1);
+            double start, end;
+            cal_3djoints(theta, d, global_3d, num);
             // Project the point to the image plane
             T tmp[4];
             T tmp2[4];
-            for (int i=0; i < joint_num; ++i) {
-                matrix_multi(MVP, T(2 * joints_2d[2*i + 1] * base_plane_width), T(2 * joints_2d[2*i] * base_plane_height), T(0), tmp2);
-                // because of the pin hole model
-                matrix_multi(MVP, global_3d[3*i], -global_3d[3*i + 1], global_3d[3*i + 2], tmp);
-                
-                residuals[2*i] = T(m_fitting_w2) * (tmp[0] - tmp2[0]);
-                residuals[2*i + 1] = T(m_fitting_w2) * (tmp[1] - tmp2[1]);
-            }
+            double base_plane_height =  5.0 * glm::tan(glm::radians(base_vof/2));
+            double base_plane_width = ratio_w * base_plane_height / ratio_h;
+            
+            matrix_multi(MVP, T(kl_x * base_plane_width), T(kl_y * base_plane_height), T(0), tmp2);
+            // because of the pin hole model
+            matrix_multi(MVP, global_3d[3*num], -global_3d[3*num + 1], global_3d[3*num + 2], tmp);
+            // I think the jacobian is just 1
+            residuals[0] = T(m_fitting_w2) * (tmp[0] - tmp2[0]);
+            residuals[1] = T(m_fitting_w2) * (tmp[1] - tmp2[1]);
             //std::cout << "X: " << tmp[0] << ", " << tmp2[0] << "\tY: " << tmp[1] << ", " << tmp2[1] << "\tZ: " << tmp[2] << ", " << tmp2[2] << "\tW: " << tmp[1] << ", " << tmp2[1] << std::endl;
             return true;
         }
-        static ceres::CostFunction * Create(double * joints_2d, glm::mat4 mvp) {
-            return (new ceres::AutoDiffCostFunction<EPROJError, 2*joint_num, joint_num*3, 3>(new EPROJError(joints_2d, mvp)));
+        static ceres::CostFunction * Create(const double kl_x, const double kl_y, int num, glm::mat4 mvp) {
+            return (new ceres::AutoDiffCostFunction<EPROJError, 2, joint_num*3, 3>(new EPROJError(kl_x, kl_y, num, mvp)));
         }
-        double * joints_2d;
+        double kl_x;
+        double kl_y;
+        int num;
         glm::mat4 MVP;
     };
+    // The one block version
+    //struct EPROJError {
+        //EPROJError(double * joints_2d, glm::mat4 mvp): joints_2d(joints_2d), MVP(mvp){};
+        //template<typename T> bool operator() (const T * const theta, const T * const d, T * residuals) const {
+            //T global_3d[joint_num*3];
+            //cal_3djoints(theta, d, global_3d, -1);
+            //// Project the point to the image plane
+            //T tmp[4];
+            //T tmp2[4];
+            //for (int i=0; i < joint_num; ++i) {
+                //matrix_multi(MVP, T(2 * joints_2d[2*i + 1] * base_plane_width), T(2 * joints_2d[2*i] * base_plane_height), T(0), tmp2);
+                //// because of the pin hole model
+                //matrix_multi(MVP, global_3d[3*i], -global_3d[3*i + 1], global_3d[3*i + 2], tmp);
+                
+                //residuals[2*i] = T(m_fitting_w2) * (tmp[0] - tmp2[0]);
+                //residuals[2*i + 1] = T(m_fitting_w2) * (tmp[1] - tmp2[1]);
+            //}
+            ////std::cout << "X: " << tmp[0] << ", " << tmp2[0] << "\tY: " << tmp[1] << ", " << tmp2[1] << "\tZ: " << tmp[2] << ", " << tmp2[2] << "\tW: " << tmp[1] << ", " << tmp2[1] << std::endl;
+            //return true;
+        //}
+        //static ceres::CostFunction * Create(double * joints_2d, glm::mat4 mvp) {
+            //return (new ceres::AutoDiffCostFunction<EPROJError, 2*joint_num, joint_num*3, 3>(new EPROJError(joints_2d, mvp)));
+        //}
+        //double * joints_2d;
+        //glm::mat4 MVP;
+    //};
     //// Punish the accelation of the change
     struct ESMOOTHError {
-        //ESMOOTHError(std::vector<double> pl_x, std::vector<double> pl_y, std::vector<double> pl_z, int num): pl_x(pl_x), pl_y(pl_y), pl_z(pl_z), num(num) {}
-        ESMOOTHError(double pl_x, double pl_y, double pl_z, int num): angle_x(pl_x), angle_y(pl_y), angle_z(pl_z), num(num) {}
+        ESMOOTHError(std::vector<double> pl_x, std::vector<double> pl_y, std::vector<double> pl_z, int num): pl_x(pl_x), pl_y(pl_y), pl_z(pl_z), num(num) {}
+        //ESMOOTHError(double pl_x, double pl_y, double pl_z, int num): angle_x(pl_x), angle_y(pl_y), angle_z(pl_z), num(num) {}
         template<typename T> bool operator() (const T * const theta, const T * const d, T * residuals) const {
-            //T global_3d[joint_num * 3];
-            //cal_3djoints(theta, d, global_3d, num);
+            T global_3d[joint_num * 3];
+            cal_3djoints(theta, d, global_3d, num);
             // Cal the acceleration
-            //residuals[0] = T(m_fitting_w3)*(T(global_3d[num * 3]) - T(pl_x[0]));
-            //residuals[1] = T(m_fitting_w3)*(T(global_3d[num * 3 + 1]) - T(pl_y[0]));
-            //residuals[2] = T(m_fitting_w3)*(T(global_3d[num * 3 + 2]) - T(pl_z[0]));
-            residuals[0] = T(m_fitting_w3) * (theta[3*num] - T(angle_x));
-            residuals[1] = T(m_fitting_w3) * (theta[3*num + 1] - T(angle_y));
-            residuals[2] = T(m_fitting_w3) * (theta[3*num + 2] - T(angle_z));
+            residuals[0] = T(m_fitting_w3)*(T(global_3d[num * 3]) - T(pl_x[0]) - d[0]);
+            residuals[1] = T(m_fitting_w3)*(T(global_3d[num * 3 + 1]) - T(pl_y[0]) - d[1]);
+            residuals[2] = T(m_fitting_w3)*(T(global_3d[num * 3 + 2]) - T(pl_z[0]) - d[2]);
+            //residuals[0] = T(m_fitting_w3) * (theta[3*num] - T(angle_x));
+            //residuals[1] = T(m_fitting_w3) * (theta[3*num + 1] - T(angle_y));
+            //residuals[2] = T(m_fitting_w3) * (theta[3*num + 2] - T(angle_z));
             return true;
         }
 
-        static ceres::CostFunction * Create(const double pl_x, const double pl_y, const double pl_z, int num) {
+        //static ceres::CostFunction * Create(const double pl_x, const double pl_y, const double pl_z, int num) {
+        static ceres::CostFunction * Create(std::vector<double> pl_x, std::vector<double> pl_y, std::vector<double> pl_z, int num) {
+            //return (new ceres::AutoDiffCostFunction<ESMOOTHError, 3, joint_num * 3, 3>(new ESMOOTHError(pl_x, pl_y, pl_z, num)));
             return (new ceres::AutoDiffCostFunction<ESMOOTHError, 3, joint_num * 3, 3>(new ESMOOTHError(pl_x, pl_y, pl_z, num)));
         }
         // Store the previous and previous previous data
-        //std::vector<double> pl_x;
-        //std::vector<double> pl_y;
-        //std::vector<double> pl_z;
-        double angle_x;
-        double angle_y;
-        double angle_z;
+        std::vector<double> pl_x;
+        std::vector<double> pl_y;
+        std::vector<double> pl_z;
+        //double angle_x;
+        //double angle_y;
+        //double angle_z;
         int num;
     };
     struct EDEPTHError {
@@ -112,7 +144,7 @@ namespace mFitting {
             T global_3d[joint_num * 3];
             cal_3djoints(theta, d, global_3d, num);
             
-            residuals[0] = T(m_fitting_w4)*(T(global_3d[num * 3 + 2]) - T(pl_z));
+            residuals[0] = T(m_fitting_w4)*(T(global_3d[num * 3 + 2]) - T(pl_z) - d[2]);
             return true;
         }
 
@@ -188,25 +220,21 @@ namespace mFitting {
     }
     void fitting(double ** joints_2d, double ** joints_3d, glm::mat4 &mvp, double ** angles, double *d) {
         ceres::Problem problem;
-        //for (int i=0; i < joint_num; ++i) {
-            //ceres::CostFunction * e1_cost_function = EIKError::Create(joints_3d[0][3*i], joints_3d[0][3*i + 1], joints_3d[0][3*i + 2], i);
-            //ceres::CostFunction * e2_cost_function = EPROJError::Create(2*joints_2d[0][2*i + 1], 2*joints_2d[0][2*i + 0], i, mvp); // cause the 2d is y, x and it's normalized to [-0.5, 0.5]
-            ceres::CostFunction * e2_cost_function = EPROJError::Create(joints_2d[0], mvp); // cause the 2d is y, x and it's normalized to [-0.5, 0.5]
-            //ceres::CostFunction * e4_cost_function = EDEPTHError::Create(joints_3d[1][3*i + 2], i);
-            //problem.AddResidualBlock(e1_cost_function, NULL, angles[0], d);
+        for (int i=0; i < joint_num; ++i) {
+            ceres::CostFunction * e1_cost_function = EIKError::Create(joints_3d[0][3*i], joints_3d[0][3*i + 1], joints_3d[0][3*i + 2], i);
+            ceres::CostFunction * e2_cost_function = EPROJError::Create(2*joints_2d[0][2*i + 1], 2*joints_2d[0][2*i + 0], i, mvp); // cause the 2d is y, x and it's normalized to [-0.5, 0.5]
+            ceres::CostFunction * e4_cost_function = EDEPTHError::Create(joints_3d[1][3*i + 2], i);
+            ceres::CostFunction * e3_cost_function = ESMOOTHError::Create(std::vector<double>({joints_3d[1][3*i], joints_3d[2][3*i]}), std::vector<double>({joints_3d[1][3*i + 1], joints_3d[2][3*i + 1]}), std::vector<double>({joints_3d[1][3*i + 2], joints_3d[2][3*i + 2]}), i);
+            problem.AddResidualBlock(e1_cost_function, NULL, angles[0], d);
             problem.AddResidualBlock(e2_cost_function, NULL, angles[0], d);
-            //problem.AddResidualBlock(e4_cost_function, NULL, angles[0], d);
-            //ceres::CostFunction * e3_cost_function = ESMOOTHError::Create(std::vector<double>({joints_3d[1][3*i], joints_3d[2][3*i]}), std::vector<double>({joints_3d[1][3*i + 1], joints_3d[2][3*i + 1]}), std::vector<double>({joints_3d[1][3*i + 2], joints_3d[2][3*i + 2]}), i);
-            //problem.AddResidualBlock(e3_cost_function, NULL, angles, d);
-            //if (i != joint_num-1) {
-                //ceres::CostFunction * e3_cost_function = ESMOOTHError::Create(angles[0][3*i], angles[0][3*i + 1], angles[0][3*i + 2], i);
-                //problem.AddResidualBlock(e3_cost_function, NULL, angles[0], d);
-            //}
-        //}
+            problem.AddResidualBlock(e4_cost_function, NULL, angles[0], d);
+            problem.AddResidualBlock(e3_cost_function, NULL, angles[0], d);
+        }
+        //ceres::CostFunction * e2_cost_function = EPROJError::Create(joints_2d[0], mvp); // cause the 2d is y, x and it's normalized to [-0.5, 0.5]
         ceres::Solver::Options option;
         option.linear_solver_type = ceres::DENSE_SCHUR;
         option.minimizer_progress_to_stdout = false;
-        option.max_num_iterations = 10;
+        option.max_num_iterations = 12;
         option.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
         ceres::Solver::Summary summary;
         ceres::Solve(option, &problem, &summary);
